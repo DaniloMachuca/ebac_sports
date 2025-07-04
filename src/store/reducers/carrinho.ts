34edba6fce029602c6700e265730d07084557baf
Danilo Machuca <danilo.machuca111@gmail.com>
2025-07-04 11:44:24 -0300

    Redux adicionado

---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { Produto } from '../../App'
+
+type CarrinhoState = {
+  itens: Produto[]
+}
+
+const initialState: CarrinhoState = {
+  itens: []
+}
+
+const carrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    adicionarAoCarrinho: (state, action) => {
+      const produto = action.payload
+
+      if (state.itens.find((p) => p.id === produto.id)) {
+        alert('Item já adicionado')
+      } else {
+        state.itens.push(action.payload)
+      }
+    }
+  }
+})
+
+export const { adicionarAoCarrinho } = carrinhoSlice.actions
+export default carrinhoSlice.reducer
